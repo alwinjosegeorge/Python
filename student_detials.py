db076afe9011da306e46825b9fcf3a8e759c5570
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-01 15:05:38 +0530

    Add files via upload

---
+++ student_detials.py
@@ -0,0 +1,7 @@
+name=input("Enter student name")
+roll_number=int(input("Enter your Roll number"))
+cgp=float(input("Enter your CGPA"))
+p=cgp*10
+print("your CGPA;",p)
+print("Nmae of student:",name)
+print("Roll Number:",roll_number)
