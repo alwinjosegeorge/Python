f505d87092bba0d4d11a5cbe881738fcfdc17838
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-01 15:39:05 +0530

    student detils with harder state

--- student_detials.py
+++ student_detials.py
@@ -1,3 +1,10 @@
+'''
+Python program to get the student details
+Author; Alwin Jose George
+Date:01-10-2024
+version: 1.0
+'''
+
 name=input("Enter student name")
 roll_number=int(input("Enter your Roll number"))
 cgp=float(input("Enter your CGPA"))
