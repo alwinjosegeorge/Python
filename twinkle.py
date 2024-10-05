bd578e323fe7f332fc789372f4617f3c3eb0ee8d
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-05 16:11:42 +0530

    Update twinkle.py

--- twinkle.py
+++ twinkle.py
@@ -1,1 +1,8 @@
+'''
+Python program to get the student details
+Author; Alwin Jose George
+Date:05-10-2024
+version: 1.0
+'''
+
 print("Twinkle, twinkle, little star, \n\t\t\tHow I wonder what you are! \n\t\t\t\t\tUp above the world so high, \n\t\t\t\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\t\t\tHow I wonder what you are!")
