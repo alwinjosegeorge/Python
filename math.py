51afb79b1338a85049307e259f88aeee4d42420a
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-05 16:05:50 +0530

    Write a Python program that uses functions from the math module to perform the following operations on a number provided by the user:

---
+++ math.py
@@ -0,0 +1,5 @@
+import math
+a=int(input(" Enter a number"))
+print("square root of",a ,":",math.sqrt(a))
+print("Factorial of ",a ,':',math.factorial(a))
+print(a,"raised to power 2:", math.pow(a,2))
