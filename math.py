6075a930c818e2242d6b8731cdeaf98100b32306
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-05 16:11:05 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,3 +1,10 @@
+'''
+Python program to get the student details
+Author; Alwin Jose George
+Date:05-10-2024
+version: 1.0
+'''
+
 import math
 a=int(input(" Enter a number"))
 print("square root of",a ,":",math.sqrt(a))
