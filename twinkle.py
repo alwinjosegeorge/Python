2767d0d146f5e2a34885981ba7e93fe5d8472102
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-05 16:07:43 +0530

    Write a Python program to print the following string in a specific format (see the output). Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :

---
+++ twinkle.py
@@ -0,0 +1,1 @@
+print("Twinkle, twinkle, little star, \n\t\t\tHow I wonder what you are! \n\t\t\t\t\tUp above the world so high, \n\t\t\t\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\t\t\tHow I wonder what you are!")
