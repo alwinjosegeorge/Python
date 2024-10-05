559845ee87ab1d31de6fea9661493288321db4f7
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-05 16:09:59 +0530

    Update input their name and age.  .py

--- input their name and age.  .py
+++ input their name and age.  .py
@@ -1,3 +1,10 @@
+'''
+Python program to get the student details
+Author; Alwin Jose George
+Date:05-10-2024
+version: 1.0
+'''
+
 name=input("Enter your name:")
 age=int(input("Enter your age:"))
 print("Hello,",name,"!", "You are ",age ," years old.")
