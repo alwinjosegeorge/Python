19a1e0dc3982b5b39a6fce8b170bf75c9f97156c
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-08 14:57:54 +0530

    Add files via upload

---
+++ Python program to split and join a string.py
@@ -0,0 +1,15 @@
+'''
+Python program to get the student details
+Author; Alwin Jose George
+Date:08-10-2024
+version: 1.0
+'''
+
+first_name="Alwin"
+last_name="Jose"
+full_name= first_name +" " +last_name
+length=len(first_name)
+extracted_last_name=full_name[length+1:]
+print(full_name)
+print(length)
+print(extracted_last_name)
