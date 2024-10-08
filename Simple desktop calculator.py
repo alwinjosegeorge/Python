9c30626f7a1135f7fac6664f23da4095f7262364
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-08 14:33:26 +0530

    Add files via upload

---
+++ Simple desktop calculator.py
@@ -0,0 +1,23 @@
+from sys import modules
+
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+num_3 = int(input("Enter third number: "))
+# Function to add two numbers
+add =num_1 + num_2
+print("The sum of num1 and num2 is:", add)
+# Function to subtract two numbers
+sub=num_2 - num_1
+print("The difference when num2 is subtracted from num1 is: ",sub)
+# Function to multiply two numbers
+mult=num_1 * num_2
+print( "The product of num1 and num2 is: ",mult)
+# Function to divide two numbers
+div=num_1 / num_2
+print("The quotient when num1 is divided by num2 is:",div)
+# Function to modulus two numbers
+modules=num_1 % num_2
+print("The remainder when num1 is divided by num2 is: ",modules)
+# Function to Combined Arithmetic Operation:
+arith=result = (num_1 + num_2) * num_3 / 2
+print("The result of (num1 + num2) * num3 / 2 is: ",arith)
