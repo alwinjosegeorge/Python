5a719dd3f952cf16d6e1b0dbd5925becb70dfa2b
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-15 14:15:14 +0530

    Add files via upload

---
+++ to check whether the given number is positive or not.py
@@ -0,0 +1,11 @@
+'''
+Python progress to check whether the given number
+is positive or not
+'''
+number=int(input("Enter a number :"))
+if number>0:
+    print("The given number:",number,"is positive")
+elif number<0:
+    print("THe given number :", number ,"is negative")
+else:
+    print("The given number:",number,"is Zero")
