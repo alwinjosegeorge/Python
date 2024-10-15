9af4fb8343729ef5ce83f021b1d2ef550f69716b
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-15 14:36:14 +0530

    Add files via upload

---
+++ Eligible for a driving license.py
@@ -0,0 +1,9 @@
+'''
+Python progress to check if a person is eligible for a diriving license
+'''
+
+age=int(input("Enter  the image:"))
+if age>=16:
+    print("Eligible for a driving license")
+else:
+    print("Not eligible for a driving license")
