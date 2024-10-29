03331997fb44f0a47589e4169e56c36892df1df7
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-10-29 14:54:29 +0530

    Add files via upload

---
+++ prime number up to n.py
@@ -0,0 +1,9 @@
+limit=int(input("Enter the limit"))
+for number in range (2,limit+1):
+    isPrime= True
+    for i in range(2,(number//2) +1):
+        if number%i==0:
+           isPrime= False
+           break
+    if isPrime:
+        print(number)
