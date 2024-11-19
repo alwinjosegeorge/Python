92e931530284113af6d69a09961858299b4b6cfc
alwinjosegeorge <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-19 10:47:03 +0530

    Add files via upload

---
+++ Tuple Fruits.py
@@ -0,0 +1,18 @@
+fruits=("apple","banana","cherry","date")
+print(fruits[0])
+print(fruits[-1])
+print(fruits[-2])
+even_numbers=(2,4,6)
+odd_numbers=(1,3,5)
+numbers=even_numbers+ odd_numbers
+print(numbers)
+repeat_tuple = ("hello",)*3
+print(repeat_tuple)
+colors=("red","green","blue","yellow","orange")
+print(colors[0:3])
+print(colors[3:5])
+person =("Ben",18,"Engineer")
+name, age, profession = person
+print(name)
+print(age)
+print(profession)
